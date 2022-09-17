bcd26ec7f5ebd9c643952c6b4c11805b4e3c389a
hussainHakimi <hussainbayan614@gmail.com>
2022-09-17 17:44:41 +0430

    store added to header.

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,8 +3,15 @@ import Image from 'next/image'
 import logo from '../assets/Logo.png'
 import Link from 'next/link'
 import { AiOutlineShopping } from 'react-icons/ai'
+import { useStore } from '../store/store'
 
+  
 const Header = () => {
+  const state = useStore((state) => state)
+  console.log(state)
+  const items = useStore((state) => state.cart.pizzas.length)
+
+
   return (
     <div className='w-full h-[100px] z-[99]'>
       <div className='flex items-center justify-between w-full h-full'>        
@@ -15,24 +22,31 @@ const Header = () => {
         
           <ul className='flex gap-[2rem] flex-1 justify-center '> {/* Menu */}
             <Link href='/'>
-              <li className='hover:text-[#F54748] font-semibold'>
+              <li className='hover:text-[#F54748] font-semibold hover:cursor-pointer'>
                 Home
               </li>
             </Link>
             <Link href='/'>
-              <li className='hover:text-[#F54748] font-semibold'>
+              <li className='hover:text-[#F54748] font-semibold hover:cursor-pointer'>
                 Menu
               </li>
             </Link>
             <Link href='/'>
-              <li className='hover:text-[#F54748] font-semibold'>
+              <li className='hover:text-[#F54748] font-semibold hover:cursor-pointer'>
                 Contact
               </li>
             </Link>
           </ul>
+
         <div className='relative flex justify-end flex-1 mx-5'> {/* Cards */}
-          <AiOutlineShopping size={35} className='text-[#2E2E2E] cursor-pointer '/>
-          <div className='absolute bg-[#F54748] text-white rounded-full text-xs w-[20px] h-[20px] text-center flex items-center justify-center top-0 right-[-8px] cursor-pointer'>1</div>
+          <Link href='/cart'>
+            <div>
+              <AiOutlineShopping size={35} className='text-[#2E2E2E] cursor-pointer '/>
+              <div className='absolute bg-[#F54748] text-white rounded-full text-xs w-[20px] h-[20px] text-center flex items-center justify-center top-0 right-[-8px] cursor-pointer'>
+                {items}
+              </div>
+          </div>
+          </Link>
         </div>
       </div>
     </div>
